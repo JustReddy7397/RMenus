db428352918aef1db2d49c1991f84e75d1f0af6b
JustReddy <j.dulk@leerling.leokanner.nl>
2022-11-25 15:50:34 +0100

    Fixed a Bug with paginated inventories.

--- src/main/java/ga/justreddy/wiki/rmenus/menu/tasks/InventoryRefreshTask.java
+++ src/main/java/ga/justreddy/wiki/rmenus/menu/tasks/InventoryRefreshTask.java
@@ -2,9 +2,16 @@ package ga.justreddy.wiki.rmenus.menu.tasks;
 
 import ga.justreddy.wiki.rmenus.MenuManager;
 import ga.justreddy.wiki.rmenus.menu.AbstractMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class InventoryRefreshTask extends BukkitRunnable {
@@ -13,7 +20,29 @@ public class InventoryRefreshTask extends BukkitRunnable {
   public void run() {
     for (Map.Entry<UUID, AbstractMenu> entry : MenuManager.getMenuManager().getOpenMenus().entrySet()) {
       if (Bukkit.getPlayer(entry.getKey()) == null) continue;
-      entry.getValue().setMenuItems(Bukkit.getPlayer(entry.getKey()));
+      final Inventory inventory = entry.getValue().getInventory();
+      for (int i = 0; i < inventory.getSize(); i++) {
+        ItemStack itemStack = inventory.getItem(i);
+        if (itemStack == null) continue;
+        if (!itemStack.hasItemMeta()) continue;
+        ItemStack clonedItem = itemStack.clone();
+        ItemMeta meta = clonedItem.getItemMeta();
+        if (itemStack.getItemMeta().hasDisplayName()) meta.setDisplayName(
+                ChatColor.translateAlternateColorCodes('&', itemStack.getItemMeta().getDisplayName())
+        );
+        if (itemStack.getItemMeta().hasLore()) meta.setLore(colorLore(itemStack.getItemMeta().getLore()));
+        clonedItem.setItemMeta(meta);
+        inventory.setItem(i, clonedItem);
+      }
     }
   }
+
+  private List<String> colorLore(List<String> input) {
+    List<String> hi = new ArrayList<>();
+    for (String i : input) {
+      hi.add(ChatColor.translateAlternateColorCodes('&', i));
+    }
+    return hi;
+  }
+
 }
